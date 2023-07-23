9adeb7d32bc5885afebf3476d1bc9606c2d5aa03
Adhiyaman  Sakthivel <adhiyamaninfotech@gmail.com>
2023-07-23 19:34:34 +0530

    issue fixing for images

--- agencyapp/urls.py
+++ agencyapp/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from django.urls import re_path
 
 
 urlpatterns = [
@@ -29,5 +31,11 @@ urlpatterns = [
     # path('api/carsouel/',include('carouselapp.urls'))
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
+# urlpatterns += [
+#     re_path(r'^static/(?P<path>.*)$', serve, {
+#         'document_root': settings.MEDIA_ROOT,
+#     }),
+# ]
