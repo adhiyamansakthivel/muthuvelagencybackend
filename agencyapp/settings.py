b21994f0f6aab9fbdc3dc16d03dbf3ae86e8c11c
Adhiyaman  Sakthivel <adhiyamaninfotech@gmail.com>
2023-07-23 18:34:11 +0530

    media root change

--- agencyapp/settings.py
+++ agencyapp/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
