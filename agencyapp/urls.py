ff7190730f06eb9b5d49cb860cb6d12bd2f648a3
Adhiyaman  Sakthivel <adhiyamaninfotech@gmail.com>
2023-07-23 18:10:10 +0530

    render.com update

--- agencyapp/urls.py
+++ agencyapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -28,4 +29,5 @@ urlpatterns = [
     # path('api/carsouel/',include('carouselapp.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
