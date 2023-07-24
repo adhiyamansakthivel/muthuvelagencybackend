c90a726d4ef8cc20e555f58ac81c32be5979b803
Adhiyaman  Sakthivel <adhiyamaninfotech@gmail.com>
2023-07-24 17:37:54 +0530

    static issue

--- agencyapp/urls.py
+++ agencyapp/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     # path('api/carsouel/',include('carouselapp.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
 
 # urlpatterns += [
